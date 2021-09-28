8fd066fec96af6a78491f1a98de433a8266ff930
Tanner Burkybile <tanner.burkybile@gmail.com>
2021-09-27 21:34:28 -0700

    edited commenet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * Description: main - Prints what assignment is asking for.
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
