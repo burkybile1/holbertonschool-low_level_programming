8d2a2bb2dbb6135f2b3fe7410e71a9204768993a
Tanner Burkybile <tanner.burkybile@gmail.com>
2021-10-03 16:03:18 -0700

    added new file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - will print the alphabet in lowercase
+ * followed by a new line.
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
+
