0ecd7b49eaa91d49c63466a1230a3e594e91d8b6
Tanner Burkybile <tanner.burkybile@gmail.com>
2021-10-03 15:30:05 -0700

    added new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - will print the alphabet in lowercase
+ * followed by a new line.
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
+
